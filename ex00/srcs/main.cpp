ed9172dc163b430b57626bff8cf2552ff7a9ab63
hugo <hdamitzian@gmail.com>
2023-11-09 03:44:36 +0100

    termine le dossier ex00 normalement tout les petits checks de merde sont ok au cas ou correcteur est un trou du cougit add .!

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 12:13:11 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/10/31 13:58:19 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/11/09 03:29:08 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ int	main(void) {
 	ClapTrap	trap1(trapName1);
 
 	trap0.beRepaired(0);
-	for (int i = 0; i < 9; i++) {
-		trap0.attack(trapName1);
-		trap1.takeDamage(trap0.getAttackDamage());
-	}
+	//for (int i = 0; i < 9; i++) {
+	//	trap0.attack(trapName1);
+	//	trap1.takeDamage(trap0.getAttackDamage());
+	//}
 	trap0.attack(trapName1);
-	trap1.takeDamage(5);
+	trap1.takeDamage(50);
 	trap1.beRepaired(4);
 	trap1.attack(trapName0);
 	trap0.takeDamage(5);
